d9d934100656cb3dc4a0ed3e8b24f1116a70255f
Milen <milen.com1@gmail.com>
2024-03-16 00:35:48 +0200

    top

--- src/views/Booking/ChooseHours.vue
+++ src/views/Booking/ChooseHours.vue
@@ -1,16 +1,32 @@
 
 <template>
 <div class="h-full w-full relative">
-    <div class="flex flex-col p-4 gap-4 h-full">
+    <div class="flex flex-col p-4 gap-4 h-full w-full">
         <div class="font-semibold">Избери час:</div>
-        <div class="border p-4 bg-white border-gray-100 flex flex-col gap-5">
+        <div class="border p-4 bg-white border-gray-100 flex flex-col gap-5 w-full overflow-x-hidden">
             <div class="flex justify-between">
                 <div class="">
                     {{monthYear}}
                 </div>
-                <div class="flex gap-6">
-                    <ChevronLeftIcon @click="state.weekDate = state.weekDate.add(5, 'days')" class="w-5 text-blue-400"/>
-                    <ChevronRightIcon @click="state.weekDate = state.weekDate.subtract(5, 'days')" class="w-5 text-blue-400"/>
+                <div class="flex gap-2">
+                    <button :disabled="state.weekDate < new Date()" class="p-4" @click="moveBack()" >
+                        <ChevronLeftIcon class="w-5 text-blue-400"/>
+                    </button>
+                    <button :disabled="moment().diff(state.weekDate, 'days') == 25" class="p-4" @click="state.weekDate = state.weekDate.add(5, 'days')" >
+                        <ChevronRightIcon class="w-5 text-blue-400"/>
+                    </button>
+                </div>
+            </div>
+            <div class="flex" :style="'transition:' + moment().diff(state.weekDate, 'days') / 5 * 100 + '%'">
+                <div v-for="i in 30"
+                     class="min-w-16 flex flex-col items-center"
+                     >
+                    <div class="text-sm text-gray-600">
+                        {{moment().add(i, 'days').locale('bg').format('ddd')}}
+                    </div>
+                    <div class="w-9 h-9 flex-center text-md">
+                        {{moment().add(i, 'days').format('D')}}
+                    </div>
                 </div>
             </div>
             <div class="h-px w-full bg-gray-100 "></div>
@@ -27,6 +43,9 @@ import {computed, reactive} from "vue";
 import * as moment from "moment/moment";
 import 'moment/locale/bg'
 import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/vue/20/solid";
+import {useBookingStore} from "../../store/booking";
+
+const bookingStore = useBookingStore();
 
 const state = reactive({
     weekDate: moment(),
@@ -34,8 +53,12 @@ const state = reactive({
 })
 
 const monthYear = computed(()=>{
-    return state.weekDate.locale("bg").format("MM YYYY");
+    return state.weekDate.locale("bg").format("YYYY MMMM");
 })
+
+const moveBack = ()=>{
+    state.weekDate = state.weekDate.add(5, 'days')
+}
 </script>
 
 <style scoped>
