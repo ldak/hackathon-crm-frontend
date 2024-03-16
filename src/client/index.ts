baaefd9a7a176c85ed0e11bb603c4e5aae6fd996
Milen <milen.com1@gmail.com>
2024-03-17 00:51:01 +0200

    fix calendar appointments

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,12 @@
 import axios, {AxiosInstance} from "axios";
 
-export const baseUrl :string = 'http://192.168.34.25:8000/'
+export const baseUrl :string = 'https://api.appointmate.me/'
 
 const httpClient :AxiosInstance = axios.create({
     baseURL: baseUrl,
     withCredentials: false,
     headers: {
-        // Accept: 'application/json',
+        Accept: 'application/json',
         // 'Content-Type': 'application/json'
     }
 });
--- src/views/Admin/Calendar.vue
+++ src/views/Admin/Calendar.vue
@@ -1,9 +1,8 @@
-
 <template>
-    <FullCalendar :options="state.calendarOptions" />
+    <FullCalendar :options="state.calendarOptions"/>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import {onMounted, reactive} from "vue";
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -11,6 +10,7 @@ import interactionPlugin from '@fullcalendar/interaction'
 import bgLocale from '@fullcalendar/core/locales/bg';
 
 import appointmentService from "../../services/appointmentService";
+import * as moment from "moment/moment";
 
 const handleDateClick = (arg) => {
     alert('date click! ' + arg.dateStr)
@@ -18,30 +18,37 @@ const handleDateClick = (arg) => {
 
 
 const state = reactive({
-    
     calendarOptions: {
-    plugins: [dayGridPlugin, interactionPlugin],
-    locale: 'bg',
-    firstDay: 1,
-    buttonText: {
-      today: 'Днес'
-    },
-    initialView: 'dayGridMonth',
+        plugins: [dayGridPlugin, interactionPlugin],
+        locale: 'bg',
+        firstDay: 1,
+        buttonText: {
+            today: 'Днес'
+        },
+        initialView: 'dayGridMonth',
         events: [],
         datesSet: async (arg) => {
             const newEvents = await appointmentService.get.getAppointments(arg.start.toJSON(), arg.end.toJSON());
             state.calendarOptions.events = newEvents.data.map((event) => {
                 return {
                     title: event.client.name + " - " + event.service.name,
-                    date: event.date
+                    date: event.date,
+                    event: event,
                 }
             });
+        },
+        eventContent: (arg) => {
+            const [
+                client_name, service_name
+            ] = arg.event.title.split('-')
+            const hour = moment(arg.event.start).format('H:mm')
+            return {html :`<div class="w-full bg-blue-400 rounded-md text-white text-bold px-1"> <div>${hour}</div><div>${client_name}</div><div>${service_name}</div></div>`}
         }
     }
 })
 
-onMounted(()=>{
-    
+onMounted(() => {
+
 })
 </script>
 
@@ -50,9 +57,10 @@ onMounted(()=>{
     text-wrap: wrap;
 }
 
-.fc .fc-button-primary{
-    background-color: rgb(37 99 235 ) !important;
+.fc .fc-button-primary {
+    background-color: rgb(37 99 235) !important;
 }
+
 .fc-day-today {
     background-color: rgb(191 219 254) !important;
 }
