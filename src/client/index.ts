ab77af2d29614127526898a3ddf8f5d1aa2b462d
Georgi Iliev <georgi.iliev533@outlook.com>
2024-03-17 15:30:52 +0200

    Fix fix

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,9 +1,5 @@
 import axios, {AxiosInstance} from "axios";
 
-<<<<<<< Updated upstream
-=======
-// export const baseUrl :string = 'http://localhost:8000/'
->>>>>>> Stashed changes
 export const baseUrl :string = 'https://api.appointmate.me/'
 
 const httpClient :AxiosInstance = axios.create({
